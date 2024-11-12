524257548a73a78e80da3ec57dbdb6112bbe2d78
stefan.bordei <stefan.bordei@softwareone.com>
2024-11-12 18:51:35 +0000

    update tmux config.

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -15,7 +15,6 @@ in
         {
 			plugin = dracula;
             extraConfig = ''
-                set -g @dracula-show-fahrenheit false
 				set -g @dracula-show-battery false
 				set -g @dracula-show-powerline true
                 set -g @dracula-refresh-rate 10
