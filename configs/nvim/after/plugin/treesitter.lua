ff64135989312ef4c527e49a16144874f814d6c9
stefan.bordei <stefan.bordei@softwareone.com>
2025-01-18 11:58:56 +0000

    update nvim config 011825.

--- configs/nvim/after/plugin/treesitter.lua
+++ configs/nvim/after/plugin/treesitter.lua
@@ -7,6 +7,9 @@ require'nvim-treesitter.configs'.setup {
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
+  indent = {
+    disable = true,
+  },
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -21,7 +24,7 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = { "python", "yaml" },
   },
   parser_install_dir = "~/.config/nvim/treesitter",
 }
